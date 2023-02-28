666f8b38f16df3b5c8a69980b415e15bd04ca14a
Alejandro Reyes <reyesbalejandroe@gmail.com>
2023-02-28 10:56:10 +0100

    update/store_folder

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import store from "./redux/store/store"
+import store from "./redux/store"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import {createStore,applyMiddleware} from 'redux'
 import logger from 'redux-logger'
 import { composeWithDevTools } from 'redux-devtools-extension';
-import cartReducer from "../cart/cartReducer";
+import cartReducer from "./cart/cartReducer";
 
 const store = createStore(cartReducer, composeWithDevTools(applyMiddleware(logger)));
 
