781772066575f66b578f9404340281bca6ce1135
Javi Espinosa <103537170+JavierEspinosaP@users.noreply.github.com>
2023-07-05 11:00:18 +0200

    add/comments

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import {createStore,applyMiddleware} from 'redux'
 import logger from 'redux-logger'
 import { composeWithDevTools } from 'redux-devtools-extension';
 import cartReducer from "./cart/cartReducer";
-
+//Aquí se crea el Store y se inicializa el estado global
 const store = createStore(cartReducer, composeWithDevTools(applyMiddleware(logger)));
 
 export default store;
